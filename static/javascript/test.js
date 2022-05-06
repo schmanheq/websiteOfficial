ae3d539a2d5c8ec6c63e35e96fa37046a58ef21e
schmanh <anhstrash@gmail.com>
2022-05-06 20:14:15 +0200

    struggles on js-button script

--- static/javascript/test.js
+++ static/javascript/test.js
@@ -1,8 +1,8 @@
-
+/*
 var more  = document.getElementById("more");
 var dots = document.getElementById("dots");
 
-function change() {
+function showMore() {
     if (dots.style.display === "block") {
         dots.style.display = "none";
         more.style.display = "block";
@@ -12,4 +12,44 @@ function change() {
         dots.style.display = "block";
         more.style.display = "none";
     }
+} */
+
+let i = 10;
+
+function myFunction() {
+  var btn = document.getElementById("mybtn");
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myBtn");
+
+  if (i > 9) {
+    dots.style.display = "inline";
+    moreText.style.display = "none";
+    i=8;
+  } else {
+    dots.style.display = "none";
+    moreText.style.display = "inline";
+    i=11;
+  }
 }
+
+ /*  
+function change() {
+  var i=0;
+  var btn = document.getElementById("btn");
+  var more = document.getElementById("more");
+  var test = document.getElementById("test");
+  
+  if (!i) {
+    more.style.display = "inline";
+    test.style.display = "none";
+    i=1;
+    console.log(i);
+  } 
+  
+  else {
+    more.style.display = "none";
+    test.style.display = "inline";
+    i=0;
+  }
+}*/
